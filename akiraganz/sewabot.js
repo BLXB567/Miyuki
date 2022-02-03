f01a2597211c1685f97921f50b664c060bc4b1c4
BLXB567 <97624291+BLXB567@users.noreply.github.com>
2022-02-03 20:16:50 +0700

    Initial commit

---
+++ akiraganz/sewabot.js
@@ -0,0 +1,15 @@
+const sewabot = () => { 
+	return `
+*OPEN JASA SEWA BOT*
+
+Sewa Sebulan : 10k
+Sewa Permanen : 20k
+
+*Payment :*
+_Gopay, Dana, dan Axis_
+
+Minat?PC
+Wa.me/6283117436733
+`
+}
+exports.sewabot = sewabot
